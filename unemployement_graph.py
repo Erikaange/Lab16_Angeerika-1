8d631f7b7355d0fee2e4478460bfc548b74ee8d0
Erikaange <akengneheukoua@student.cscc.edu>
2025-05-04 22:04:40 -0400

    added title, colors and style to the graph and finished commenting the code

--- unemployement_graph.py
+++ unemployement_graph.py
@@ -1,8 +1,15 @@
+# name: Lab16_Angeerika-1.py
+# author: Angeerika
+# comment: This program reads a CSV file containing unemployment data and plots it using matplotlib.
+# date: 05/03/25
+
+
+
 from pathlib import Path
 import matplotlib.pyplot as plt
 import csv
 from datetime import datetime
-# This program reads a CSV file containing unemployment data and plots it using matplotlib.
+
 
 path = Path('OHUR (1).csv')
 lines = path.read_text(encoding='utf-8').splitlines()
@@ -15,9 +22,12 @@ header_row = next(reader)
 for index, col_title in enumerate(header_row): #analyses the data and gives the index and title of each column
     print(f'{index} {col_title}, ' , end=' ')
 
+# Prepare lists to store dates and unemployment rates
 dates = []   
 unemployment_rate = []
 
+
+# Extract data from each row in the CSV
 for row in reader:
     current_date = datetime.strptime(row[0], '%Y-%m-%d')
     date = current_date
@@ -26,11 +36,20 @@ for row in reader:
     unemployment_rate.append(unemployment)
 
  # graphing the processed data
-
-plt.style.use('dark_background')
+plt.style.use('ggplot')# Set the plot style
 figure, graph = plt.subplots()
-graph.plot(dates, unemployment_rate, color= 'magenta')
-plt.show()   
+graph.plot(dates, unemployment_rate, color= 'magenta')# Plot dates (x-axis) against unemployment rates (y-axis)
+graph.fill_between(dates, unemployment_rate, color='blue', alpha=0.2) #fill the area under the line
+
+# Add title and axis label
+graph.set_title('Unemployment Rate Timeline', fontsize=18)
+graph.set_ylabel('Rates(%)', fontsize=16)
+graph.set_xlabel('Date', fontsize=16)
+
+figure.autofmt_xdate() #fix the date
+
+plt.show()   # Display the plot
+
     
 
 
