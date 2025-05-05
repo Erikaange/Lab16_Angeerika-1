e2d41b88bd5a0aa52982c4625fb0dc994c8897d2
Erikaange <akengneheukoua@student.cscc.edu>
2025-05-04 21:24:15 -0400

    analyzed the header information usinf enumerate()

--- unemployement_graph.py
+++ unemployement_graph.py
@@ -8,4 +8,14 @@ lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
-print(header_row)
+# print(header_row)
+
+for index, col_title in enumerate(header_row): #analyses the data and gives the index and title of each column
+    print(f'{index} {col_title}, ' , end=' ')
+print()    
+
+
+
+
+
+
